72389f38d5a20061c749cc80399137f18519c646
Subhadip Jana <subhadipjana69@gmail.com>
2024-07-06 00:19:26 +0530

    Set up GitHub Actions for deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Hrx/', // Ensure this matches your repository name
+  base: '/Hrx-Website/', // Ensure this matches your repository name
   plugins: [react()],
 });
