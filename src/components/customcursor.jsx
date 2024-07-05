72b283ca60f8ae90acfd970ce8c4f71d83584b61
Amit Bhandari <ab1679322@gmail.com>
2024-07-05 22:22:28 +0530

    added custom cursor when hovering over a video

--- src/components/customcursor.jsx
+++ src/components/customcursor.jsx
@@ -5,8 +5,10 @@ const CustomCursor = () => {
   useEffect(() => {
     const cursorDot = document.querySelector("[data-cursor-dot]");
     const cursorOutline = document.querySelector("[data-cursor-outline]");
+    let playButton = null;
 
     const handleMouseMove = (e) => {
+      document.body.style.cursor = 'none';
       const posX = e.clientX;
       const posY = e.clientY;
 
@@ -20,6 +22,41 @@ const CustomCursor = () => {
         duration: 963,
         fill: "forwards"
       });
+      // Detect if cursor is over a video
+  const elementUnderCursor = document.elementFromPoint(posX, posY);
+  if (elementUnderCursor && elementUnderCursor.tagName.toLowerCase() === 'video') {
+    cursorDot.style.opacity = '0';  // Make the dot invisible
+    cursorOutline.classList.add('play-button');
+    
+    // Add the play button div if it doesn't exist
+    if (!playButton) {
+      playButton = document.createElement('div');
+      playButton.id = 'play-button';
+      document.body.appendChild(playButton);
+      
+    }
+
+    // Position the play button div at the cursor
+    playButton.style.left = `${posX}px`;
+    playButton.style.top = `${posY}px`;
+    playButton.style.position = 'fixed';
+    playButton.style.zIndex = '9999';
+    playButton.style.pointerEvents = 'none';
+    playButton.style.width = '0.8rem';
+    playButton.style.height = '0.8rem';
+    playButton.style.clipPath = 'polygon(20% 0%, 100% 50%, 20% 100%)';
+    playButton.style.backgroundColor = 'white';
+    playButton.style.transform = 'translate(-50%, -50%)';
+  } else {
+    cursorDot.style.opacity = '1';  // Make the dot visible
+    cursorOutline.classList.remove('play-button');
+
+    // Remove the play button div if it exists
+    if (playButton) {
+      document.body.removeChild(playButton);
+      playButton = null;
+    }
+  }
     };
 
     window.addEventListener("mousemove", handleMouseMove);
